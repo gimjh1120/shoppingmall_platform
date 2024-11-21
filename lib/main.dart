20d895ae08dbe9df9fcdf858df55aca5104afc02
jae_brother <gimjh1120@gmail.com>
2024-11-21 16:36:14 +0900

    [add] 페이지 세팅

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shoppingmall_platform/list/product_list.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,6 +11,8 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      home: ProductList(),
+    );
   }
 }
